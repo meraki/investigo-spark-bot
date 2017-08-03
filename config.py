93184ecedf3f4b040f098a2ad00ffe2b22258f08
Rafael Carvalho <rafael.lcarvalho@gmail.com>
2017-08-03 15:22:23 -0700

    parsing boolean on app vars

--- config.py
+++ config.py
@@ -14,8 +14,10 @@ class Config(object):
     ON_CISCO_NETWORK = os.environ.get('ON_CISCO_NETWORK', False)
     NOTIFICATION_SMS_PHONE_NUMBER = os.environ.get('NOTIFICATION_SMS_PHONE_NUMBER', False)
     SPARK_DEFAULT_ROOM_ID = os.environ.get('SPARK_DEFAULT_ROOM_ID', False)
-    SMS_ENABLED = bool(os.environ.get('SMS_ENABLED', False))
-    SHOW_WEB_LINK = bool(os.environ.get('SHOW_WEB_LINK', False))
+    SMS_ENABLED = os.environ.get('SMS_ENABLED', False)
+    SMS_ENABLED = (SMS_ENABLED == 'True' or SMS_ENABLED == 'TRUE')
+    SHOW_WEB_LINK = os.environ.get('SHOW_WEB_LINK', False)
+    SHOW_WEB_LINK = (SHOW_WEB_LINK == 'True' or SHOW_WEB_LINK == 'TRUE')
     ADMIN_NAME = os.environ.get('ADMIN_NAME', '')
     MERAKI_VALIDATOR_TOKEN = os.environ.get('MERAKI_VALIDATOR', "TOKEN-NOT-PROVIDED")
 
